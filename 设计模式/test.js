16b10f3c291fa91055156d528815c7c2a6396fa4
sunhaifeng <imsunhf@gmail.com>
2018-04-13 18:24:10 +0800

    reading

--- 设计模式/test.js
+++ 设计模式/test.js
@@ -1,53 +1,29 @@
-var pubsub = {};
-(function (q) {
+function Parent () {
+    this.name = 'parent'
+    this.play = [1,3,4]
+}
+Parent.prototype.say = [1,1,1]
+function Child () {
+    Parent.call(this)
+    this.name = 'child'
+}
+function Child1 () {    
+    Parent.call(this)
+    this.name= 'child1'
+}
+Child.prototype = Object.create(Parent.prototype)
+Child.prototype.constructor = Child
 
-    var topics = {}, // 回调函数存放的数组
-        subUid = -1;
-    // 发布方法
-    q.publish = function (topic, args) {
+Child1.prototype = Object.create(Parent.prototype)
+Child1.prototype.constructor = Child1
 
-        if (!topics[topic]) {
-            return false;
-        }
+var child = new Child()
+var child1 = new Child1()
 
-        setTimeout(function () {
-            var subscribers = topics[topic],
-                len = subscribers ? subscribers.length : 0;
+child.play.push(2)
+console.log(child.play)
+console.log(child1.play)
 
-            while (len--) {
-                subscribers[len].func(topic, args);
-            }
-        }, 0);
-
-        return true;
-
-    };
-    //订阅方法
-    q.subscribe = function (topic, func) {
-
-        if (!topics[topic]) {
-            topics[topic] = [];
-        }
-
-        var token = (++subUid).toString();
-        topics[topic].push({
-            token: token,
-            func: func
-        });
-        return token;
-    };
-    //退订方法
-    q.unsubscribe = function (token) {
-        for (var m in topics) {
-            if (topics[m]) {
-                for (var i = 0, j = topics[m].length; i < j; i++) {
-                    if (topics[m][i].token === token) {
-                        topics[m].splice(i, 1);
-                        return token;
-                    }
-                }
-            }
-        }
-        return false;
-    };
-} (pubsub));
+child.say.push(2)
+console.log(child.say)
+console.log(child1.say)
